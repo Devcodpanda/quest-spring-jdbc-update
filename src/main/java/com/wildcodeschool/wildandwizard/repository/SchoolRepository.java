02828a3987800fa6bc499189678216a207fd269d
Valou <devcodpanda@gmail.com>
2020-06-22 18:08:58 +0200

    resolv update school

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,10 +1,6 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 import com.wildcodeschool.wildandwizard.entity.School;
 import com.wildcodeschool.wildandwizard.util.JdbcUtils;
@@ -12,12 +8,38 @@ import com.wildcodeschool.wildandwizard.util.JdbcUtils;
 public class SchoolRepository {
 
     private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
-    private final static String DB_USER = "h4rryp0tt3r";
+    private final static String DB_USER = "h4rrypOtt3r";
     private final static String DB_PASSWORD = "Horcrux4life!";
 
     public School update(Long id, String name, Long capacity, String country) {
 
         // TODO : update a school from the database
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "UPDATE school SET name=?, capacity=?, country=? WHERE id=?"
+            );
+            statement.setString(1, name);
+            statement.setLong(2, capacity);
+            statement.setString(3, country);
+            statement.setLong(4, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to update data");
+            }
+            return new School(id, name, capacity, country);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeConnection(connection);
+            JdbcUtils.closeStatement(statement);
+
+        }
         return null;
     }
 
